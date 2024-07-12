f5475ab21a166ec063c63207d18eedd66816bab6
jumarno.mwb <jumarno.mwb@gmail.com>
2024-07-12 16:47:23 +0000

    Sign In Function

--- lib/page/signin_page.dart
+++ lib/page/signin_page.dart
@@ -1,6 +1,9 @@
+import 'package:d_info/d_info.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/config/app_asset.dart';
 import 'package:myapp/config/app_color.dart';
+import 'package:myapp/config/app_route.dart';
+import 'package:myapp/source/user_source.dart';
 import 'package:myapp/widget/button_custom.dart';
 
 class SigninPage extends StatelessWidget {
@@ -8,8 +11,23 @@ class SigninPage extends StatelessWidget {
 
   final controllerEmail = TextEditingController();
   final controllerPassword = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
-  login(BuildContext context) {}
+  login(BuildContext context) {
+    if (formKey.currentState!.validate()) {
+      UserSource.signIn(controllerEmail.text, controllerPassword.text)
+          .then((response) {
+        if (response['success']) {
+          DInfo.dialogSuccess(context, response['message']);
+          DInfo.closeDialog(context, actionAfterClose: () {
+            Navigator.pushReplacementNamed(context, AppRoute.home);
+          });
+        } else {
+          DInfo.toastError(response['message']);
+        }
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,91 +38,103 @@ class SigninPage extends StatelessWidget {
             constraints: BoxConstraints(
               minHeight: constraints.maxHeight,
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    AppAsset.logo,
-                    width: 180,
-                    fit: BoxFit.fitWidth,
-                  ),
-                  const SizedBox(
-                    height: 80,
-                  ),
-                  Text(
-                    'Sign In\nTo Your Account',
-                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                          fontWeight: FontWeight.bold,
+            child: Form(
+              key: formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      AppAsset.logo,
+                      width: 180,
+                      fit: BoxFit.fitWidth,
+                    ),
+                    const SizedBox(
+                      height: 80,
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Sign In\nTo Your Account',
+                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      controller: controllerEmail,
+                      validator: (value) =>
+                          value!.isEmpty ? 'Email tidak boleh kosong' : null,
+                      decoration: InputDecoration(
+                        isDense: true,
+                        filled: true,
+                        fillColor: Colors.white,
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 16),
+                        hintText: 'Email Address',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
                         ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    controller: controllerEmail,
-                    decoration: InputDecoration(
-                      isDense: true,
-                      filled: true,
-                      fillColor: Colors.white,
-                      contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 16),
-                      hintText: 'Email Address',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: const BorderSide(color: AppColor.secondary),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide.none,
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              const BorderSide(color: AppColor.secondary),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    controller: controllerPassword,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      isDense: true,
-                      filled: true,
-                      fillColor: Colors.white,
-                      contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 16),
-                      hintText: 'Password',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: const BorderSide(color: AppColor.secondary),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide.none,
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      controller: controllerPassword,
+                      validator: (value) =>
+                          value!.isEmpty ? 'Password tidak boleh kosong' : null,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        isDense: true,
+                        filled: true,
+                        fillColor: Colors.white,
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 16),
+                        hintText: 'Password',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              const BorderSide(color: AppColor.secondary),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  ButtonCustom(
-                      label: 'Sign In',
-                      isExpand: true,
-                      onTap: () => login(context)),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  const Text(
-                    'Create New Account',
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                ],
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    ButtonCustom(
+                        label: 'Sign In',
+                        isExpand: true,
+                        onTap: () => login(context)),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Text(
+                      'Create New Account',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
---
+++ lib/source/user_source.dart
@@ -0,0 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:myapp/config/session.dart';
+import 'package:myapp/model/user.dart';
+
+class UserSource {
+  static Future<Map<String, dynamic>> signIn(
+      String email, String password) async {
+    Map<String, dynamic> response = {};
+    try {
+      final credential = await auth.FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      response['success'] = true;
+      response['message'] = 'Sign In Success';
+      String uid = credential.user!.uid;
+      User user = await getWhereId(uid);
+      Session.saveUser(user);
+    } on auth.FirebaseAuthException catch (e) {
+      response['success'] = false;
+      if (e.code == 'user-not-found') {
+        response['message'] = 'User Not Found';
+      } else if (e.code == 'wrong-password') {
+        response['message'] = 'Wrong Password';
+      } else {
+        response['message'] = 'Sign In Failed';
+      }
+    }
+    return response;
+  }
+
+  static Future<User> getWhereId(String id) async {
+    DocumentReference<Map<String, dynamic>> ref =
+        FirebaseFirestore.instance.collection('users').doc(id);
+    DocumentSnapshot<Map<String, dynamic>> doc = await ref.get();
+    return User.fromJson(doc.data()!);
+  }
+}
